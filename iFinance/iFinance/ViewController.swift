edb63c6e936de1e70d787af4ef2ef75b538b141a
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-04-11 22:00:52 +0900

    initiate Project

---
+++ iFinance/iFinance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iFinance
+//
+//  Created by 김윤석 on 2022/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
